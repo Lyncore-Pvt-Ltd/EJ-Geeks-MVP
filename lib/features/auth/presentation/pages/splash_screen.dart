bd2ebd065b423cc5d910a0b52bafba6308b9756f
Arshvin Waduge <tarshvin@gmail.com>
2026-07-20 02:02:39 +0530

    initiate the file to configure the splace screen

---
+++ lib/features/auth/presentation/pages/splash_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  static MaterialPageRoute route() {
+    return MaterialPageRoute(builder: (context) => const SplashScreen());
+  }
+
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
