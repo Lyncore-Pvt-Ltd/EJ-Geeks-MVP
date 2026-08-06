f1d7eed9bd9cfaa10904c7f0e9562c4711cd7aea
Arshvin Waduge <tarshvin@gmail.com>
2026-08-06 22:32:13 +0530

    Optimize the app for small screens and add the client's web address

--- lib/features/dashboard/presentation/pages/home_screen.dart
+++ lib/features/dashboard/presentation/pages/home_screen.dart
@@ -68,6 +68,7 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
               const SizedBox(height: 20),
               const RevenueChartCard(),
+              SizedBox(height: 100 + MediaQuery.of(context).padding.bottom),
             ],
           ),
         ),
